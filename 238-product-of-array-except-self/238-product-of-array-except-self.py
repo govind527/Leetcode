690e853e2c8eb6049255155b0298ade36e6c34af
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-13 22:48:56 +0530

    added comments in

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,9 +1,11 @@
+# Notes:make two passes, first in-order, second in-reverse, to compute products
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res=[1]* len(nums)
+        res=[1]* len(nums) # in starting we take array of 1's
         prefix=1
         for i in range(len(nums)):
-            res[i]=prefix
+            res[i]=prefix   3# storing in res list 
             prefix=prefix*nums[i]
         postfix=1
         for i in range(len(nums)-1,-1,-1):
