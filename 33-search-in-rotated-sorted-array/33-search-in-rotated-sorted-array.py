98bba23d0719971650c63fb970502fb93ead838f
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-17 23:39:25 +0530

    Time: 76 ms (18.17%), Space: 14.2 MB (86.96%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,32 @@
+#Notes :at most two sorted halfs, mid will be apart of left sorted or right sorted, if target is in range of sorted portion then search it, otherwise search other half
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l=0
+        r=len(nums)-1
+        while(l<=r):
+            m=l+(r-l)//2
+            
+            if target==nums[m]:
+                return m
+            
+            #left sorted array
+            if nums[l]<=nums[m]:
+                if target>nums[m]:
+                    l=m+1
+                elif target<nums[l]:
+                    l=m+1
+                else:
+                    r=m-1
+            
+            
+            else:
+                if target<nums[m]:
+                    r=m-1
+                elif target>nums[r]:
+                    r=m-1
+                else:
+                    l=m+1
+        return -1
+                
+        
