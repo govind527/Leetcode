b52d716bcedd42e05a2ce757c7700cf3cd6c07bb
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-23 23:18:57 +0530

    add comment

--- 90-subsets-ii/90-subsets-ii.py
+++ 90-subsets-ii/90-subsets-ii.py
@@ -1,7 +1,9 @@
+# this solution is same as solution of subset-i
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
         subset=set()
-        nums.sort()
+        nums.sort()	    # Sort to assist check for adjacent elements to avoid duplicates
+
         for i in range(2**len(nums)):
             sub=[]
             for j in range(len(nums)):
