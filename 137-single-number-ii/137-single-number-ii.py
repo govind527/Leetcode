8a145c2bf12ec845b5e77dafb27aa23d58f9e8f3
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-24 23:44:07 +0530

    Time: 83 ms (63.18%), Space: 15.7 MB (74.00%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -7,9 +7,24 @@ class Solution:
 		# (3x+3y+3z) - (3x+3y+z) = 2z
 		# div the ans by 2 = 2z/2 = z--> our ans
 
-		# return (3 * sum(set(nums)) - sum(nums)) // 2
-        for i in nums:
-            if nums.count(i)==1:
-                return i
+		# return (3 * sum(set(nums)) - sum(nums)) // 24
+        
+        
+        # for i in nums:
+        #     if nums.count(i)==1:
+        #         return i
+        one, two, three = 0, 0, 0
+        for num in nums:
+		
+            two |= (one & num) # this bit show up once previosuly, and recorded in one time, and second time in  num
+            one ^= num
+            
+            three = two & one # show up both in one time & two time
+            
+            # reset twos if this bit show up three times total
+            two = two & (~three)
+            one = one & (~three)
+        
+        return one
         
         
