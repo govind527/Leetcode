cb550ccac8851970e628dbfb4b00ac2a5213171b
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-24 23:39:55 +0530

    Time: 1473 ms (6.24%), Space: 15.8 MB (74.00%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.py
@@ -0,0 +1,15 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # Let the numbers be x,y,z,.....
+		# require sum should be 3x+3y+3z
+		# original sum = 3x+3y+z
+		# Subtract require sum from original sum
+		# (3x+3y+3z) - (3x+3y+z) = 2z
+		# div the ans by 2 = 2z/2 = z--> our ans
+
+		# return (3 * sum(set(nums)) - sum(nums)) // 2
+        for i in nums:
+            if nums.count(i)==1:
+                return i
+        
+        
