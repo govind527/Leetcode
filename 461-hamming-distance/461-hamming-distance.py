ebe7b589ca404198bbb2173c6f2db98f6a5fadb5
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-02-27 01:06:56 +0530

    added comments

--- 461-hamming-distance/461-hamming-distance.py
+++ 461-hamming-distance/461-hamming-distance.py
@@ -1,3 +1,5 @@
+#Notes:We need to count number of positions at which the bits differ for given numbers x and y. 
+#For this, we can use bit-wise XOR. The xor of two bits is set only if they differ. So, we need to xor and count the number of set bits in it.
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         xor=x^y
