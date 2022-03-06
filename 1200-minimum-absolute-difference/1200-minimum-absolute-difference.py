20a773d7dc74fb01bc7671bbf46af4eaf903d329
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-03-06 23:55:19 +0530

    Time: 476 ms (55.82%), Space: 28.2 MB (59.88%) - LeetHub

---
+++ 1200-minimum-absolute-difference/1200-minimum-absolute-difference.py
@@ -0,0 +1,28 @@
+class Solution:
+    def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
+        
+        n=len(arr)
+        #if n<=1: return []
+        
+        ## We sort the array because pairs of elements with the minimum absolute difference will definitely 
+        ## be next to each other in the sorted array.
+        arr.sort()
+        
+        min_diff=float("inf")
+        ans_pairs=[]
+        
+        for i in range(1,n):
+            
+            curr_diff = arr[i]-arr[i-1]
+            curr_pair = [ arr[i-1], arr[i]]
+            
+            if curr_diff < min_diff:
+                min_diff = curr_diff
+                ans_pairs = [ curr_pair ]
+                
+            elif curr_diff == min_diff:
+                ans_pairs.append( curr_pair )
+                
+        return ans_pairs
+        
+        
