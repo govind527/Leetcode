79a28a78307ba784dd16bfeaf65dc50863558fb0
Govind Kumar <62988424+govind527@users.noreply.github.com>
2022-09-05 10:49:34 +0530

    Time: 1355 ms (14.42%), Space: 0B (100.00%) - LeetHub

---
+++ 196-delete-duplicate-emails/196-delete-duplicate-emails.sql
@@ -0,0 +1,9 @@
+/* 
+ Please write a DELETE statement and DO NOT write a SELECT statement.
+ Write your T-SQL query statement below
+ */
+delete p2
+from Person p1
+join Person p2
+on p1.email=p2.email
+where p1.id<p2.id
